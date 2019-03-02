e665987f42b8c2d979aae8e4b7733d6dabec0b9d
kenny mendez <kennymendez10@gmail.com>
2019-03-02 01:41:52 -0500

    m

---
+++ db/seeds.sql
@@ -0,0 +1,10 @@
+-
+Select the burgers_db
+USE burgers_db;
+-- Insert new rows of data.
+INSERT INTO burgers
+    (burger_name, devoured)
+VALUES
+    ("Sashimi Burger", false),
+    ("French Burger", false),
+    ("Sunset Burger", false);
